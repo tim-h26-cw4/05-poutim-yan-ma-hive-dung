368a5c876659dadb8be4ed88534d9404bb68f15f
YuAreNarukami8 <yanmahive@gmail.com>
2026-03-04 14:02:39 -0500

    Incomplet compliquer

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -3,13 +3,53 @@ export default class Chef {
   constructor(element) {
     this.element = element;
     this.menu = [];
-    this.container = document.querySelectorAll('.js-container');
+    this.options = {
+      total: 0,
+      poulet: 1,
+      touskiL: 1,
+      saucisse: 1,
+      addition: 0,
+    };
+    this.order = document.querySelectorAll('.js-submit');
+    this.container = document.querySelector('.js-container');
     this.init();
   }
   init() {
-    const instance = new Poutine(this.element);
-    this.menu.push(instance);
-    this.element.addEventListener('click', this.sendOrder.bind(this));
+    const poutines = this.element.querySelectorAll('.js-poutine');
+    for (let i = 0; i < poutines.length; i++) {
+      const poutine = poutines[i];
+      const instance = new Poutine(poutine);
+      this.menu.push(instance);
+    }
+
+    for (let i = 0; i < this.order.length; i++) {
+      const orders = this.order[i];
+      orders.addEventListener('click', this.sendOrder.bind(this));
+    }
   }
-  sendOrder() {}
+  sendOrder() {
+    this.poulet = document.querySelectorAll('.js-type');
+    this.active = document.querySelectorAll('.is-active');
+    for (let i = 0; i < this.active.length; i++) {
+      let actives = this.active[i];
+      actives = true;
+
+      for (let i = 0; i < this.poulet.length; i++) {
+        const poulets = this.poulet[i];
+        if ('poulet' in poulets.dataset && actives == true) {
+          this.options.total = this.options.poulet;
+        }
+        if ('touski' in poulets.dataset) {
+          this.options.total = 1;
+        }
+        if ('saucisse' in poulets.dataset) {
+          this.options.total = 1;
+        }
+      }
+    }
+    this.container.innerHTML = '';
+    const p = document.createElement('p');
+    p.innerText = `Nombre total de poutine(s) : ${this.options.total}`;
+    this.container.appendChild(p);
+  }
 }
--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -2,35 +2,33 @@ export default class Poutine {
   constructor(element) {
     this.element = element;
     this.types = document.querySelectorAll('.js-type');
-    this.options = {
-      name: 'poulet',
-    };
+
     this.selectedType = '';
 
     this.init();
   }
   init() {
-    const boutons = document.querySelectorAll('.button');
-    for (let i = 0; i < boutons.length; i++) {
-      const bouton = boutons[i];
+    for (let i = 0; i < this.types.length; i++) {
+      const bouton = this.types[i];
       bouton.addEventListener('click', this.selectType.bind(this));
     }
   }
 
   selectType(event) {
-    const bouton = event.currentTarget;
+    this.bouton = event.currentTarget;
+
     for (let i = 0; i < this.types.length; i++) {
       const type = this.types[i];
       type.classList.remove('is-active');
-      this.selectedType = event.currentTarget.innerText;
     }
-    bouton.classList.add('is-active');
+    this.bouton.classList.add('is-active');
+    this.selectedType = this.bouton.innerText;
     this.updatePhoto();
   }
   updatePhoto() {
-    const image = document.querySelector('.js-image');
-    image.classList.add('is-active');
+    const image = this.element.querySelector('.js-image');
 
+    image.classList.add('is-active');
     image.src = `assets/images/${this.selectedType}.png`;
   }
 }
